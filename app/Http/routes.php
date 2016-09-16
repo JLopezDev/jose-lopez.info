ba1fe36cb4f64f8d911f254bf591e71d09be6b9a
Jose Lopez <Jose@up3.com>
2016-09-16 05:06:36 -0400

    installed laravel 5.1.33 installed cv template

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,15 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you will register all of the routes in an application.
+| Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
 Route::get('/', function () {
-    return view('coming-soon');
+    return view('welcome');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,9 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    mix
+        .browserSync({proxy: 'joselopez.app'})
+        .sass('app.scss');
+
 });
